d7fe17afc9fbcd73546aba669c6ef5e55364f94e
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-21 17:36:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,11 +6,11 @@ class Application
     resp = Rack::Response.new
     time = Time.now
     
-    if time < 12
-      resp.write "Good Morning!"
-    else
-      resp.write "Good Afternoon!"
-    end
+    # if time < 12
+    #   resp.write "Good Morning!"
+    # else
+    #   resp.write "Good Afternoon!"
+    # end
     resp.finish
   end
 end
